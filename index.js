af57d761c0d2cd82ed4900281ba46df5018d215f
Jeremiah Newman <jeremiahsm1@Jeremiahs-MacBook-Pro.local>
2022-01-27 10:58:31 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,13 @@
+// import our restaurants
+const restaurants = require('./restaurants.json');
+
+// check if this is correct by console.log()
+console.log(restaurants[0].name)
+//expected value 'Bolay'
+
+
+
+// connect to Firestore
+// create a collection called "restaurants"
+// add each restaurant
+
